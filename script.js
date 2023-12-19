492ae5842867a32b0191d64dbb82502440811759
Ian Santos <64675360+IanSantosOS@users.noreply.github.com>
2023-12-19 10:31:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const http = require("http");
 const port = 80;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(`
-        <h1>Belligol, Belligol, Bellingham!!!</h1>
-        <img height="400" src="https://i.ytimg.com/vi/C9q6gWttToI/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDHWaiaCdqYRuC1dt6dh-Wb5wB8pg"/>
-        <img height="400" src="https://footure.com.br/wp-content/uploads/2023/09/Bellingham-Real-Madrid-260823.jpg"/>
-    `);
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`
+    <div style="text-align: center; margin-top: 1em">
+      <h1>Belligol, Belligol, Bellingham!!!</h1>
+      <img height="400" src="https://i.ytimg.com/vi/C9q6gWttToI/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDHWaiaCdqYRuC1dt6dh-Wb5wB8pg"/>
+      <img height="400" src="https://footure.com.br/wp-content/uploads/2023/09/Bellingham-Real-Madrid-260823.jpg"/>
+    </div>
+  `);
 });
 
 server.listen(port, () => {
