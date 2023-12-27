afe316866bf3de75120e8aa87fc51a1ba8ce4f29
Ian Santos <64675360+IanSantosOS@users.noreply.github.com>
2023-12-27 11:03:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,48 +8,71 @@ const PORT = 3000;
 
 // Criando o Servidor WEB
 const server = http.createServer((req, res) => {
-    // Cabeçalho
-    res.writeHead(200, {'Content-Type': 'text/html'});
+  // Cabeçalho
+  res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    const parseURL = url.parse(req.url, true);
+  const parseURL = url.parse(req.url, true);
 
-    let html = "<h1>Titulo Multiplicacao</h1>"
-    if (parseURL.pathname === '/tabela') {
-        const valor = parseURL.query.numero
-        html += '<table border="1">'
-        for (let i = 0; i <= 10; i++) {
-            html +=
-            `<tr>
-            <td>${valor} X ${i}</td>
-            <td>${valor * i}</td>
-            </tr>`
-        }
-        html += "</table>"
+  let html =
+  `
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #eeeeee;
+      }
+      table {
+        width: 95px;
+        background-color: #ffffff;
+      }
+    </style>
+  `
+  html += "<h1>Tabela Multiplicacao</h1>"
+
+  if (parseURL.pathname === '/') {
+    html += '<p>Entre em <a href="/tabela">/tabela</a> para ver a tabuada</p>'
+    html += '<p> escreva: <a href="/tabela?num=1">/tabela?num=1</a> para uma tabuada expecífica</p>'
+    html += '<p> escreva: <a href="/tabela?num=1">/tabela?num=1</a> para uma tabuada expecífica</p>'
+  }
+  else if (parseURL.pathname === '/tabela') {
+    const valor = Number(parseURL.query.num);
+
+    if (parseURL.search) {
+      html += '<table border="1">'
+
+      for (let i = 0; i <= 10; i++) {
+        html += '<tr>'
+        html += `<td>${valor} X ${i}</td>`
+        html += `<td>${valor * i}</td>`
+        html += '</tr>'
+      }
+
+      html += "</table>";
     }
     else {
-        html = '<div style="display: flex">'
-        
-        for (let j = 0; j <= 10; j++) {
-            html += '<table border="1">'
-            for (let i = 0; i <= 10; i++) {
-                html +=
-                `<tr>
-                <td>${j} X ${i}</td>
-                <td>${j * i}</td>
-                </tr>`
-            }
-            html += "</table>"
+      html += '<div style="display: flex; gap: 1em; flex-wrap: wrap">';
+
+      for (let j = 0; j <= 10; j++) {
+        html += '<table border="1">';
+
+        for (let i = 0; i <= 10; i++) {
+          html += '<tr>';
+          html += `<td>${j} X ${i}</td>`;
+          html += `<td>${j * i}</td>`;
+          html += '</tr>';
         }
 
-        html = '</div>'
+        html += "</table>";
+      }
+
+      html += '</div>';
     }
+  }
 
-
-    // Corpo da resposta
-    res.end(html);
+  // Corpo da resposta
+  res.end(html);
 });
 
 // Ativando o Servidor WEB
 server.listen(PORT, HOST, () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
