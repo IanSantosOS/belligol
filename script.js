ad03dbac2ddd351bd7f12d35a21450e1cfe82b4c
Ian Santos <64675360+IanSantosOS@users.noreply.github.com>
2023-12-27 11:03:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 const http = require("http");
-const PORT = 80;
+const port = 80;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(`
-      <style>
-        img {
-          height: 192.5px;
-        }
-      </style>
-      <h1>Belligol, Belligol, Bellingham!!!</h1>
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`
+    <style>
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box
+      }
+      html, body { height: 100dvh; width: 100% }
+      h1 { padding: 0 10px 10px 10px }
+      img { height: 144.7px }
+      body {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        overflow: hidden;
+        padding: 10px;
+      }
+      iframe {
+        width: 635.86px;
+        border: none;
+        height: 100%;
+        width: 479.16px;
+      }
+      .images {
+        display: flex;
+        margin-bottom: 5px;
+        gap: 5px;
+      }
+    </style>
+    <h1>Belligol, Belligol, Bellingham!!!</h1>
+    <div class="images">
       <img src="https://i.ytimg.com/vi/C9q6gWttToI/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDHWaiaCdqYRuC1dt6dh-Wb5wB8pg"/>
       <img src="https://footure.com.br/wp-content/uploads/2023/09/Bellingham-Real-Madrid-260823.jpg"/>
-      <br>
-      <iframe title="DVD" src="https://pranx.com/bouncing-dvd-screensaver/" width="100%" height="73%"></iframe>
-    `);
+    </div>
+    <iframe title="DVD" src="https://pranx.com/bouncing-dvd-screensaver/"></iframe>
+  `);
 });
 
-server.listen(PORT, () => {
-  console.log(`Servidor escutando em http://localhost:${PORT} `);
+server.listen(port, () => {
+  console.log(`Servidor escutando em http://localhost:${port}`);
 });
